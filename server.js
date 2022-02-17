0518bdefe4f8abdccc5108cd4b1a59547cdd13a9
Arthur Bernier Jr <ma2bernier@gmail.com>
2022-02-16 19:19:24 -0500

    Added Gitignore and installed dotenv

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const fruits = require('./models/fruits'); // importing fruits from wherever it is
+const reactViews = require('express-react-views')
+const createEngine = reactViews.createEngine
+const renderFile = createEngine()
+
+
+app.set('view engine', 'jsx');
+app.engine('jsx', renderFile);
+
+// MIDDLEWARE 
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    console.log(req.body)
+    next()
+})
+
+//INDEX week10day3
+app.get('/fruits', (req, res) => {
+    res.render('fruits/Index', { fruits })
+});
+
+//NEW week 11day01
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/New')
+})
+
+
+//DELETE
+
+//UPDATE
+
+//CREATE week 11 day 1
+
+app.post('/fruits', (req, res) => {
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    fruits.push(req.body);
+    res.redirect('/fruits');
+})
+
+
+//EDIT
+
+//SHOW week10day3
+
+app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+   res.render('fruits/Show', {
+       fruit: fruits[req.params.indexOfFruitsArray]
+   })
+})
+
+
+app.listen(3000, () => {
+    console.log('If your reading this Im still listening');
+})
---
+++ views/fruits/Show.jsx
@@ -0,0 +1,17 @@
+const React = require('react');
+
+class Show extends React.Component {
+    render(){
+        const fruit = this.props.fruit;
+        return (
+            <div>
+                <h1>This is the Show Page for {fruit.name.toUpperCase()} </h1>
+                <a href="/fruits">Go Back to the index</a>
+                <p>{fruit.name} is the color of {fruit.color}</p>
+                <p>The {fruit.name} is {fruit.readyToEat ? 'ready to eat' : 'not ready to eat... Can\'t touch this'}</p>
+            </div>
+        );  
+    }
+}
+
+module.exports = Show;
